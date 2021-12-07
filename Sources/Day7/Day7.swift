f6dcc2a5a5603a0533930263d6997d5d89f22918
Sean McMains <sean@mcmains.net>
2021-12-07 16:09:01 -0600

    Day 7, Part 2

--- Sources/Day7/Day7.swift
+++ Sources/Day7/Day7.swift
@@ -1,22 +1,25 @@
 import Algorithms
 
+typealias FuelUseFunction = (Int, Int) -> Int
+let startingPositions = input.split(separator: ",").map{ Int($0)! }
+let min = startingPositions.min()!
+let max = startingPositions.max()!
+
 public func partOne() {
-    let startingPositions = input.split(separator: ",").map{ Int($0)! }
-    print(bestPosition(with: startingPositions))
+    print(bestPosition(with: startingPositions, fuelUse: { abs($0 - $1) }))
 }
 
-func fuelCost(for targetPosition: Int, with positions: [Int]) -> Int {
+func fuelCost(for targetPosition: Int, with positions: [Int], fuelUse: FuelUseFunction) -> Int {
     positions.reduce(0) { partialResult, currentPosition in
-        partialResult + abs(currentPosition - targetPosition)
+        partialResult + fuelUse(currentPosition, targetPosition)
     }
 }
 
-func bestPosition(with positions: [Int]) -> Int {
-    let min = positions.min()!
-    let max = positions.max()!
+func bestPosition(with positions: [Int], fuelUse: FuelUseFunction) -> Int {
 
     let best = (min...max).reduce((position: -1, cost: Int.max)) { partialResult, i in
-        let cost = fuelCost(for: i, with: positions)
+        print("\(Float(i)/Float(max)) completed")
+        let cost = fuelCost(for: i, with: positions, fuelUse: fuelUse)
         if cost < partialResult.cost {
             return (position: i, cost: cost)
         } else {
@@ -28,5 +31,20 @@ func bestPosition(with positions: [Int]) -> Int {
 }
 
 public func partTwo() {
+    let maxDistance = max - min
+    let lookupTable: [Int] = (0...maxDistance).map { i in
+        guard i > 0 else { return 0}
+        var tally = 0
+        (1...i).forEach { tally += $0 }
+        return tally
+    }
 
+    print(
+        bestPosition(
+            with: startingPositions,
+            fuelUse: {
+                lookupTable[ abs($0 - $1) ]
+            }
+        )
+    )
 }
